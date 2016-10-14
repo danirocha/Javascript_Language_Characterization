628ffa89a66b159b37e92425c9cadc5e6c0fe13e
danirs <danirs@br.ibm.com>
2016-10-13 23:32:50 -0300

    Start finding path to extract file dinamically

--- app.js
+++ app.js
@@ -1,64 +1,56 @@
 var exec = require('child_process').exec;
+var Finder = require('fs-finder');
 // get package name from cmd (IMPLEMENTAR AINDA!!!!!!!!!!)
-var pckg = 'jquery';
-// get package's .js files destination (PEGAR DINAMICAMENTE!!!!!!!!!)
-var fileSrc = '.\\node_modules\\'+pckg+'\\dist\\';
+var pckg = 'swiper';
+var fileSrc, fileMinified;
 
+// Install specified package by npm
 function downloadPackage() {
   var cmd = 'npm install '+pckg;
-// execute 'cmd' command to install package by npm
+
   function callback(error, stdout, stderr) {
-    console.log(stdout);
-    var fileMinified = pckg+'.min.js';
-    extractJSFile(fileSrc, fileMinified);
+    if(stdout) {
+      console.log('package '+pckg+' installed!');
+
+      fileMinified = pckg+'.min.js';
+      findFile();
+    }
   }
   exec(cmd, callback);
 }
 
-function extractJSFile(fileSrc, fileMinified) {
-  var cmd = 'move '+fileSrc+fileMinified+' .\\src';
-  // execute 'cmd' command and moves wanted .js file to a ./dist/packageName directory
+// Find package.min.js file's path
+function findFile() {
+  var files = Finder.from('./node_modules').findFiles('<'+fileMinified+'$>', function(paths) {
+    fileSrc = paths;
+    console.log("path to extract: "+fileSrc);
+    extractFile();
+  });
+}
+
+// Moves wanted .min.js file to a ./src directory
+function extractFile() {
+  var cmd = 'move '+fileSrc+' .\\src';
+
   function callback(error, stdout, stderr) {
-    console.log(stdout);
-    uninstallPackage();
+    if(stdout){
+      console.log(fileMinified+' file extracted to .\\src');
+      uninstallPackage();
+    }
   }
   exec(cmd, callback);
 }
 
+// Uninstall specified package by npm
 function uninstallPackage() {
   var cmd = 'npm uninstall '+pckg;
-  // execute 'cmd' command to uninstall package by npm
+
   function callback(error, stdout, stderr) {
-    console.log(stdout);
+    if(stdout)
+      console.log('package '+pckg+' uninstalled!');
   }
   exec(cmd, callback);
 }
 
+// init
 downloadPackage();
-
-// download and write the .tgz file to extract src code
-// function getPackageSrcCode(packageUrl) {
-//   var output = pckg+".tgz";
-//
-//   request({url: packageUrl, encoding: null}, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       fs.writeFile(output, body, function(err) {
-//         console.log("package "+pckg+".tgz downloaded!");
-//         extractTarball(output);
-//       });
-//     }
-//     else
-//       console.log(error);
-//   });
-// }
-
-// function extractTarball(packageName) {
-//   var dist = './dist/'+pckg;
-//
-//   targz().extract('./'+packageName, dist, function(err) {
-//     if(err)
-//       console.log(err.stack);
-//     else
-//       console.log('package '+pckg+'.tgz extracted to '+dist);
-//   });
-// }
