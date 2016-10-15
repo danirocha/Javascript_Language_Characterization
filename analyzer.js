5322d59d1e2f1cc2c4970b2101db0f011c9e439d
Luis Felipe <sobral93@gmail.com>
2016-10-15 18:27:49 -0300

    Adição do programa de análise dos códigos

---
+++ analyzer.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var path = require("path");
+var esprima = require('esprima');
+var estraverse = require('estraverse');
+
+var p = "\src";
+fs.readdir(p, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(p, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+        fs.readFile(file, function ( err, logData ) {
+
+            if ( err ) throw err;
+
+            var text = logData.toString();
+
+            var syntax = esprima.parse(text, {loc: true} );
+            var linhaFinal = 0;
+    
+            estraverse.traverse(syntax, {
+                enter: function(node) {
+                    var linha = node.loc.end.line;
+
+                    if(linha > linhaFinal)
+                        linhaFinal = linha;
+                }
+            });
+
+            console.log("Número de linhas total do código " + file + ": " + linhaFinal);
+        });
+    });
+});
