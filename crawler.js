35d2a6e07b038205216db4c5bb03c9b1ffc2b8c5
Daniela Rocha <danirocha.rmd@gmail.com>
2016-11-21 00:53:13 -0200

    Add insert packaegs project

--- crawler.js
+++ crawler.js
@@ -2,26 +2,53 @@ var request = require('request');
 var cheerio = require('cheerio');
 var Jetty = require("jetty");
 var fs = require('fs');
-// var URL = require('url-parse');
+var db = require('./packagesDB.js');
 
 var jetty = new Jetty(process.stdout);
 var search = "a";
 var pageCount = 1;
 var pageToVisit = "https://www.npmjs.com/search?q=" + search + "&page=";
-var $, totalResults, totalPages, packages;
+var $, $github, totalResults, totalPages, packages,
+    options = {
+              host     : 'localhost',
+              user     : 'tcc_user',
+              password : 'tccstorage',
+              database : 'my_db'
+            };
 
-// function getJSONData() {
-//   var allData;
-//
-//   fs.readFile('./data.json', function(err, data) {
-//       if (err)
-//           throw err;
-//       allData = JSON.parse(data.toString());
-//       // return allData;
-//   });
-//
-//   return allData === undefined? []:allData;
-// }
+function getPackagesInfos() {
+    var array = [
+        $(this).find(".name").text(), // name
+        $(this).find(".author").text(), // author
+        Number($(this).find(".stars").text()), // stars
+        $(this).find(".version").text(), // version
+        $(this).find(".description").text(), // description
+        $(this).find(".keywords").text().replace(/\s/g,'') // tags
+    ];
+
+    // get github url
+    var url = "https://www.npmjs.com/package/"+array[0];
+    var matched = false;
+
+    request(url, function(error, response, body) {
+        if (error)
+            console.log("Error: " + error);
+        // Check status code (200 is HTTP OK)
+        if (response.statusCode === 200)
+            $github = cheerio.load(body);
+
+        var elem = $github('.box li a');
+        elem.each(function(index) {
+            var urlAttr = elem[index].attribs.href;
+            if ((urlAttr.indexOf("github") !== -1) && !matched) {
+                matched = true;
+                array.push(urlAttr);
+                packages.push(array);
+                // db.addPackage(array);
+            }
+        });
+    });
+}
 
 // Get all search results into individual objects
 function getAllSearchResults() {
@@ -29,33 +56,15 @@ function getAllSearchResults() {
     jetty.moveTo([4,0]);
     jetty.text("page "+pageCount+": "+percent+"% read");
 
-    if (pageCount <= totalPages) {
-        fs.readFile('./data.json', function(err, data) {
-            if (err)
-                throw err;
-            packages = JSON.parse(data.toString());
-
-            jetty.text(" ("+packages.length+"/"+totalResults+")");
-            var searchResult = $('.search-results .package-details');
-            searchResult.each(function(index) {
-                var obj = {
-                    name: $(this).find(".name").text(),
-                    author: $(this).find(".author").text(),
-                    stars: Number($(this).find(".stars").text()),
-                    version: $(this).find(".version").text()
-                };
-                if(packages.indexOf(obj) === -1)
-                  packages.push(obj);
-            });
-            // console.log(packages, packages.length);
-            // jetty.text(" ("+packages.length+"/"+totalResults+")");
-            fs.writeFileSync('./data.json', JSON.stringify(packages), 'utf-8');
-            pageCount++;
-            requestPage(pageToVisit, getAllSearchResults);
-        });
+    if (pageCount <= 40) {
+        packages = [];
+        var searchResult = $('.search-results .package-details');
+        searchResult.each(getPackagesInfos);
     }
-    else
+    else {
+        db.closeDB();
         console.log("\nDone! all results for '"+search+"' were captured.\n ");
+    }
 }
 
 // Get total pages number
@@ -83,6 +92,7 @@ function requestPage(pageURL, callback) {
 function init() {
     console.log("\nsearching page for '" + search + "'...\n ");
     requestPage(pageToVisit, getPageNums);
+    db.connectDB(options);
 }
 
 // init
